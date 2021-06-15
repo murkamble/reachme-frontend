69947eb368d1e5730c6870780b6bf099e3509666
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-06-15 12:12:24 +0530

    App layout setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { Route, Switch } from "react-router-dom"
 import './App.css'
 
 class App extends React.Component {
   render(){
     return (
-      <div>ReachMe</div>
+      <div>
+        ReachMe
+      </div>
     )
   }
 }
