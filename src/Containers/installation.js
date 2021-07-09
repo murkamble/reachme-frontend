b07092e8eed52a6e32ee3d69678d4e4a720f35be
cse-mkamble <cse.mkamble@gmail.com>
2021-07-09 18:38:28 +0530

    Installation Page

--- src/Containers/installation.js
+++ src/Containers/installation.js
@@ -1,14 +1,36 @@
 import React from "react"
+import { useSelector } from 'react-redux'
 
-class Installation extends React.Component {
-    render(){
-        return(
+// class Installation extends React.Component {
+    const Installation = () => {
+
+        const { theme } = useSelector(state => state)
+
+        // render() {
+        return (
             <div>
-                Installation
-                flipkart similar to Reachme
+                <div>
+                    <div style={{ display: 'flex', justifyContent: 'center' }} >
+                        <div style={{ paddingTop: '15px' }}>
+                            <h4 style={{ marginBottom: '.0rem' }}>Installation</h4>
+                        </div>
+                    </div>
+                    <hr />
+                    <div>
+                        <p>Please follow this steps.</p>
+                        <ol>
+                            <li>1) In the top right, click the Customize and control Google Chrome icon ( three dot ).</li>
+                            <div style={{ width: '100%', padding: '10px' }}><img style={{ width: '100%', filter: theme ? 'invert(1)' : 'invert(0)'}} src="https://res.cloudinary.com/mayurkamble/image/upload/v1625834596/how-to-home-button-google-chrome-1_m7rf4s.jpg" /></div>
+                            <li>2) Then click Install App.</li>
+                            <div style={{ width: '100%', padding: '10px' }}><img style={{ width: '100%', filter: theme ? 'invert(1)' : 'invert(0)'}} src="https://res.cloudinary.com/mayurkamble/image/upload/v1625834614/WhatsApp_Image_2021-07-09_at_18.12.00_p2no3e.jpg" /></div>
+                            <li>3) Finally, Click on Install.</li>
+                            <div style={{ width: '100%', padding: '10px' }}><img style={{ width: '100%', filter: theme ? 'invert(1)' : 'invert(0)'}} src="https://res.cloudinary.com/mayurkamble/image/upload/v1625834623/WhatsApp_Image_2021-07-09_at_18.12.00_1_lsbggu.jpg" /></div>
+                        </ol>
+                    </div>
+                </div>
             </div>
         )
     }
-}
+// }
 
 export default Installation
